f34ae3a042a00f4337afffe4dbfb9154a2bae73a
fghwett <1058178245@qq.com>
2022-10-20 13:06:45 +0800

    活跃时间段更新限制

--- core/liveness.go
+++ core/liveness.go
@@ -12,7 +12,9 @@ type lnClient struct {
 	inc      float64 // 增量活跃
 	interval float64 // 活跃间隔
 
-	lastUpdateTime     time.Time
+	startTime          time.Time // 有效活跃开始时间
+	endTime            time.Time // 有效活跃结束时间
+	lastUpdateTime     time.Time // 上次更新时间
 	updateLivenessFunc func(float64)
 
 	mu sync.Mutex
@@ -27,15 +29,42 @@ func NewLnClient(liveness float64, updateLivenessFunc func(float64)) *lnClient {
 		lastUpdateTime:     time.Now().Add(-30 * time.Second),
 		updateLivenessFunc: updateLivenessFunc,
 	}
+	ln.calcValidTime()
 
 	go ln.watch()
 	return ln
 }
 
-func (ln *lnClient) Say() {
+func (ln *lnClient) calcValidTime() {
+	now := time.Now()
+	ln.startTime = time.Date(now.Year(), now.Month(), now.Day(), 8, 0, 0, 0, now.Location())
+	ln.endTime = time.Date(now.Year(), now.Month(), now.Day(), 19, 30, 0, 0, now.Location())
+	fmt.Printf("今日活跃时间：%s ~ %s\n", ln.startTime.Format("2006-01-02 15:04:05"), ln.endTime.Format("2006-01-02 15:04:05"))
+}
+
+func (ln *lnClient) isContinue() bool {
+	now := time.Now()
+	if now.Year() != ln.startTime.Year() || now.Month() != ln.startTime.Month() || now.Day() != ln.startTime.Day() {
+		// 天数变化
+		ln.liveness = 0
+		ln.calcValidTime()
+		return true
+	}
+	if now.Before(ln.startTime) || now.After(ln.endTime) {
+		return false
+	}
 	if ln.liveness >= 100 {
+		return false
+	}
+
+	return true
+}
+
+func (ln *lnClient) Say() {
+	if !ln.isContinue() {
 		return
 	}
+
 	now := time.Now()
 	if now.Sub(ln.lastUpdateTime).Seconds() < 30 {
 		return
@@ -67,8 +96,7 @@ func (ln *lnClient) Say() {
 func (ln *lnClient) watch() {
 	ticker := time.NewTicker(time.Minute * 10)
 	for {
-		if ln.liveness == 100 {
-			ticker.Stop()
+		if ln.liveness >= 100 {
 			return
 		}
 		select {
